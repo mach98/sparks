7d15801c8737daaa268c90805bc7a4ff8ba19c38
Williams Bolade <dbolade2000@gmail.com>
2024-08-07 21:06:58 +0100

    katex now working

--- src/components/QuestionItem.tsx
+++ src/components/QuestionItem.tsx
@@ -1,4 +1,10 @@
-import { View, Text, Image, useWindowDimensions, Button } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  useWindowDimensions,
+  TouchableOpacity,
+} from 'react-native';
 import React, { FC, ReactElement } from 'react';
 import RenderHTML, {
   CustomBlockRenderer,
@@ -20,17 +26,37 @@ interface QuizItemProps {
 const QuestionItem: FC<QuizItemProps> = ({ question, onAnswerChange }) => {
   const { width, height } = useWindowDimensions();
 
+  const getPrefix = (index: number): string => {
+    const prefixes = ['A', 'B', 'C'];
+    return prefixes[index] || '';
+  };
+
   const renderMathContent = (content: string) => {
+    const escapeHtml = (unsafe: string) => {
+      return unsafe
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+    };
+    const escapeContent = escapeHtml(content);
     const htmlContent = `
       <!DOCTYPE html>
       <html>
       <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.18/katex.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.18/katex.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" integrity="sha384-nB0miv6/jRmo5UMMR1wu3Gz6NLsoTkbqJghGIsx//Rlm+ZU03BU6SQNC66uf4l5+" crossorigin="anonymous">
+        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.js" integrity="sha384-7zkQWkzuo3B5mTepMUcHkMB5jZaolc2xDwL6VFqjFALcbeS9Ggm/Yr2r3Dy4lfFg" crossorigin="anonymous"></script>
+        <style>
+        #content {
+          font-size: 3em;
+
+        }
+      </style>
         <script>
           document.addEventListener("DOMContentLoaded", function() {
             var content = "${content.replace(/"/g, '\\"')}";
-            var html = katex.renderToString(content, { throwOnError: false });
+            var html = katex.renderToString(content, { throwOnError: false, trusts:true });
             document.getElementById('content').innerHTML = html;
           });
         </script>
@@ -44,11 +70,11 @@ const QuestionItem: FC<QuizItemProps> = ({ question, onAnswerChange }) => {
   };
 
   return (
-    <View key={question.id} className={`mt-7 w-[${width}*0.9] self-center`}>
+    <View key={question.id} className='mt-5 self-center'>
       <WebView
         originWhitelist={['*']}
         source={{ html: renderMathContent(question.content) }}
-        className={`w-[${width}*0.9] h-[${height} * 0.3]`}
+        style={{ width: width * 0.9, height: 10, alignItems: 'center' }}
       />
       {question.type === 'image' && (
         <Image
@@ -56,6 +82,25 @@ const QuestionItem: FC<QuizItemProps> = ({ question, onAnswerChange }) => {
           className={`w-[${width * 0.8}] h-[${height}*0.3]`}
         />
       )}
+      {question.options?.map((option, index) => (
+        <TouchableOpacity
+          className='flex-row mt-2 py-1 items-center'
+          key={index}
+        >
+          <Text className='mr-3 font-semibold text-lg'>
+            {getPrefix(index)}.
+          </Text>
+          <WebView
+            originWhitelist={['*']}
+            source={{ html: renderMathContent(option) }}
+            style={{
+              width: width * 0.9,
+              height: height * 0.001,
+              alignItems: 'center',
+            }}
+          />
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
